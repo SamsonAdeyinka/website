862c869dd93717cd12a33d7bdef2844eaf33042b
Samson Adeyinka <samsonadeyinka@hotmail.co.uk>
2024-01-29 22:44:33 +0000

    back to coding 2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ArtComponent } from './pages/art/art.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'art', component: ArtComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact-me', component:  ContactMeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
